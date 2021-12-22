dcb940cf97f8e22cbcaf3032acdd4eb4b44687dd
seohyun319 <tjguselly0319@gmail.com>
2021-12-22 22:12:27 +0900

    1. splash 만들기

--- FireBase/HowlTalk/app/src/main/java/com/example/howltalk/SplashActivity.java
+++ FireBase/HowlTalk/app/src/main/java/com/example/howltalk/SplashActivity.java
@@ -1,14 +1,78 @@
 package com.example.howltalk;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        linearLayout = (LinearLayout)findViewById(R.id.splashactivity_linearlayout);
+//        구성 개체 인스턴스 가져오고 캐시 빈번하게 새로고침할 수 있도록 설정
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+//        xml 파일에서 기본값 설정 (만들었던 default_config로 설정)
+        mFirebaseRemoteConfig.setDefaults(R.xml.default_config);
+
+//        원격 구성 서버에서 값 가져오는 fetch()요청 만들고 activateFetched() 호출해 해당 값 앱에 적용
+//        (서버에 값이 있을 경우 서버값 덮어씌움)
+        mFirebaseRemoteConfig.fetch(0) //시간. 3600이면 1시간마다 요청
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+
+
+                            // Once the config is successfully fetched it must be activated before newly fetched
+                            // values are returned.
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+
+                        }
+                        displayMessage();
+                    }
+                });
+    }
+
+    void displayMessage() {
+        String splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        boolean caps = mFirebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = mFirebaseRemoteConfig.getString("splash_message");
+
+        linearLayout.setBackgroundColor(Color.parseColor(splash_background));
+
+        if(caps){ //경고창
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(splash_message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+
+            builder.create().show(); //확인 누르면 꺼지게
+
+        }
+
     }
 }
