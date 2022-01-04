d03e68663433404ea9e5aea0166032165f7cd964
seohyun319 <tjguselly0319@gmail.com>
2022-01-04 23:37:36 +0900

    1. 회원가입 및 레이아웃 꾸미기 - 회원가입Join 로그인 페이지 꾸미기

--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
@@ -1,12 +1,31 @@
 package com.example.mysolelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+//        setContentView(R.layout.activity_intro) (여기 지워도 됨)
+        // 바인딩해주기
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        //버튼에 클릭 이벤트 연결
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
