dc8110d605a46258d5a7048d7571f72cc99ff49f
seohyun319 <tjguselly0319@gmail.com>
2022-01-05 13:57:52 +0900

    2. 회원가입 로직 구현 - Firebase 세팅 & Firebase 회원가입 구현

--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
@@ -1,12 +1,79 @@
 package com.example.mysolelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.mysolelife.MainActivity
 import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityJoinBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding : ActivityJoinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        binding.joinBtn.setOnClickListener {
+            var isGoToJoin = true
+
+            val email = binding.emailArea.text.toString()
+            val password1 = binding.passwordArea1.text.toString()
+            val password2 = binding.passwordArea2.text.toString()
+
+            // input창 값이 비어있는지 확인
+            if(email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+            if(password1.isEmpty()) {
+                Toast.makeText(this, "Password1을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+            if(password2.isEmpty()) {
+                Toast.makeText(this, "Password2을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            // 비밀번호 2개(password1과 비밀번호 확인창인 password2)가 같은지 확인
+            if(!password1.equals(password2)) {
+                Toast.makeText(this, "비밀번호를 똑같이 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            // 비밀번호가 6자 이상인지
+            if (password1.length < 6) {
+                Toast.makeText(this, "비밀번호를 6자리 이상으로 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(isGoToJoin) { // 앞의 조건들에 걸리지 않아서 isGoToJoin이 True일 때 firebase에 정보 넣어줌줌
+               auth.createUserWithEmailAndPassword(email, password1).addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+
+                        // 로그인 성공하면 MainActivity로 이동
+                        val intent = Intent(this, MainActivity::class.java)
+                        // 회원가입 성공하고 MainActivity로 갔을 떄 뒤로가기 누르면 다시 회원가입창으로 가면 안 됨. 앱이 꺼지게 함.
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+
+                    } else {
+                        Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+        }
     }
 }
