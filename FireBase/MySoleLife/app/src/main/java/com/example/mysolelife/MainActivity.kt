f0d12ecb1d3895464fc1fe156bad96feab7823b9
seohyun319 <tjguselly0319@gmail.com>
2022-01-05 14:21:30 +0900

    2. 회원가입 로직 구현 - 로그인, 로그아웃, 익명로그인

--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/MainActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.mysolelife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener{
+            auth.signOut()
+
+            //로그아웃하면 인트로 액티비티로 이동
+            val intent = Intent(this, IntroActivity::class.java)
+            // 기존에 쌓인 액티비티는 날려주겠다
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
@@ -3,16 +3,26 @@ package com.example.mysolelife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.mysolelife.MainActivity
 import com.example.mysolelife.R
 import com.example.mysolelife.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     private lateinit var binding : ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_intro) (여기 지워도 됨)
+
+        auth = Firebase.auth
+
         // 바인딩해주기
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
@@ -25,6 +35,22 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        // 비회원 가입
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
 
     }
