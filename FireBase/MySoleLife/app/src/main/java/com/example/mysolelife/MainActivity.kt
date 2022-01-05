12eefa88f81f85ac2733002ded37bc53ce5e92ba
seohyun319 <tjguselly0319@gmail.com>
2022-01-05 14:51:31 +0900

    3. 메인화면 레이아웃 꾸미기 - 메인화면 꾸미기

--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/MainActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/MainActivity.kt
@@ -19,14 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        findViewById<Button>(R.id.logoutBtn).setOnClickListener{
-            auth.signOut()
-
-            //로그아웃하면 인트로 액티비티로 이동
-            val intent = Intent(this, IntroActivity::class.java)
-            // 기존에 쌓인 액티비티는 날려주겠다
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        }
+//        findViewById<Button>(R.id.logoutBtn).setOnClickListener{
+//            auth.signOut()
+//
+//            //로그아웃하면 인트로 액티비티로 이동
+//            val intent = Intent(this, IntroActivity::class.java)
+//            // 기존에 쌓인 액티비티는 날려주겠다
+//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            startActivity(intent)
+//        }
     }
 }
--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/SplashActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/SplashActivity.kt
@@ -5,16 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//        splash 3초(3000) 후에 끝내고 intro로 연결
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        auth = Firebase.auth
+
+
+        if(auth.currentUser?.uid == null) { //auth.currentUser?.uid가 비었다면. 로그인 안 한 사람이라면.
+            // splash 3초(3000) 후에 끝내고 intro로 연결
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+        } else { //auth.currentUser.uid 현재 유저의 uid가 있다면. 로그인했던 사람이라면.
+            // splash 3초(3000) 후에 끝내고 main으로 연결
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
+
     }
 }
