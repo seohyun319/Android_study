cd19de2a2764624d4cd18de3a7ad7e94198973ab
seohyun319 <tjguselly0319@gmail.com>
2022-01-06 23:03:51 +0900

    3. 메인화면 레이아웃 꾸미기 - 네비게이션 구현

---
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/fragments/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.mysolelife.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.mysolelife.R
+import com.example.mysolelife.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.tipTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
+        }
+        binding.talkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+        }
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+        }
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
+        }
+        return binding.root
+    }
+}
