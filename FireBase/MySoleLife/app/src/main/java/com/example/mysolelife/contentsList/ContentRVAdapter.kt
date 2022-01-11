abf4515359ff7fdc2fa080db3527df83bff597f7
seohyun319 <tjguselly0319@gmail.com>
2022-01-11 23:20:32 +0900

    4. 팁 페이지 만들기 - 콘텐츠 리스트 만들기 - Glide로 외부 이미지 넣기

--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/contentsList/ContentRVAdapter.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/contentsList/ContentRVAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.mysolelife.contentsList
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mysolelife.R
 
-class ContentRVAdapter(val items: ArrayList<ContentModel>) : RecyclerView.Adapter<ContentRVAdapter.Viewholder>() {
+class ContentRVAdapter(val context : Context, val items: ArrayList<ContentModel>) : RecyclerView.Adapter<ContentRVAdapter.Viewholder>() {
     // 아이템 하나(content_rv_item.xml) 갖고와서 하나의 레이아웃 만들어줌
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
@@ -29,8 +32,14 @@ class ContentRVAdapter(val items: ArrayList<ContentModel>) : RecyclerView.Adapte
         fun bindItems(item: ContentModel) {
             // itemView = content_rv_item.xml임. (위에서 연결해줬음.)
             val contentTitle = itemView.findViewById<TextView>(R.id.textArea)
+            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+
             // item은 `val items: ArrayList<ContentModel>`로 받아온 items의 각각 하나의 아이템
             contentTitle.text = item.title
+
+            Glide.with(context)
+                .load(item.imageUrl)
+                .into(imageViewArea)
         }
     }
 
