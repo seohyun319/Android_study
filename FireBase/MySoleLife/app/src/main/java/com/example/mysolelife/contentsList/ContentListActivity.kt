9bbbf64bdc6b8a9d5feb2e00fe1f54a1090c8de8
seohyun319 <tjguselly0319@gmail.com>
2022-01-12 00:20:16 +0900

    4. 팁 페이지 만들기 - Firebase 데이터 추가

--- FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/contentsList/ContentListActivity.kt
+++ FireBase/MySoleLife/app/src/main/java/com/example/mysolelife/contentsList/ContentListActivity.kt
@@ -5,14 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mysolelife.R
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ContentListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_list)
+
+        // Firebase Realtime Database
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("contents")
+
+        myRef.push().setValue(
+            ContentModel("title5", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png", "https://philosopher-chan.tistory.com/1235?category=941578")
+        )
+        myRef.push().setValue(
+            ContentModel("title6", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png", "https://philosopher-chan.tistory.com/1236?category=941578")
+        )
+        myRef.push().setValue(
+            ContentModel("title7", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png", "https://philosopher-chan.tistory.com/1237?category=941578")
+        )
+        myRef.push().setValue(
+            ContentModel("title8", "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOYyBM%2Fbtq67Or43WW%2F17lZ3tKajnNwGPSCLtfnE1%2Fimg.png", "https://philosopher-chan.tistory.com/1238?category=941578")
+        )
 
         // -------------------------------------------------------------------------------------------------
         // ContentListActivity ⊃ ContentRVAdapter
